44746fdd6f119212dd356d1ab55f51db9e2daf8b
clanand11 <122421143+clanand11@users.noreply.github.com>
2023-01-30 01:04:00 +0530

    Add files via upload

--- dbconnection.php
+++ dbconnection.php
@@ -4,10 +4,6 @@
         $conn = new SQLite3('database.db');
         return $conn;
     }
-    function flush_database()
-    {
-        exec("python init_db.py");
-    }
     function CloseCon($conn)
     {
         $conn -> close();
